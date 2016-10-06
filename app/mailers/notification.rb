cc50a790e4f7064ab6d118be43afc5979c2ed078
Steve Ellis <email@steveell.is>
2016-10-06 12:45:04 -0400

    make email notifications configurable

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -1,11 +1,18 @@
 class Notification < ActionMailer::Base
 
-  default from: 'notifications@smartcontract.com'
+  default from: ENV['EMAIL_USERNAME']
 
   def ethereum_balance(account, balance)
     @account = account
     @balance = balance
-    mail(to: ['team@smartcontract.com'], subject: "(#{Rails.env}) Low Ethereum balance alert!")
+    mail(to: notification_addresses, subject: "(#{Rails.env}) Low Ethereum balance alert!")
+  end
+
+
+  private
+
+  def notification_addresses
+    ENV['NOTIFICATION_EMAIL'].split(',').map(&:strip)
   end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  require 'dotenv-rails'
 end
