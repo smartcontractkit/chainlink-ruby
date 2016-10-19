1632c05d536dbc963246330c6a2aca07784c2549
Steve Ellis <email@steveell.is>
2016-10-19 13:05:27 -0400

    scope spec helpers

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,57 +1,59 @@
-def payment_factory(options = {})
-  expectation = options[:payment_expectation] || payment_expectation_factory
-  output = options[:bitcoin_output] || factory_create(:bitcoin_output)
+module SpecHelpers
+  def payment_factory(options = {})
+    expectation = options[:payment_expectation] || payment_expectation_factory
+    output = options[:bitcoin_output] || factory_create(:bitcoin_output)
 
-  expectation.payments.create({
-    bitcoin_output: output,
-    bitcoin_transaction: output.bitcoin_transaction
-  })
-end
-
-def term_factory(options = {})
-  factory_create :term, options
-end
-
-def custom_term_factory(options = {})
-  options[:contract] ||= custom_contract_factory
-  term_factory options
-end
-
-def oracle_term_factory(options = {})
-  oracle_contract_factory.terms.first.tap do |term|
-    term.update_attributes options
+    expectation.payments.create({
+      bitcoin_output: output,
+      bitcoin_transaction: output.bitcoin_transaction
+    })
   end
-end
 
-def contract_factory(options = {})
-  status = options[:status] || Contract::IN_PROGRESS
-  json_body = options[:json_body] || contract_json
-  body = JSON.parse json_body
-
-  ContractBuilder.perform(body).tap do |contract|
-    contract.status = status
-    contract.save if contract.persisted?
+  def term_factory(options = {})
+    factory_create :term, options
   end
-end
 
-def custom_contract_factory(options = {})
-  options[:json_body] ||= contract_json term: custom_term_json
-  contract_factory options
-end
+  def custom_term_factory(options = {})
+    options[:contract] ||= custom_contract_factory
+    term_factory options
+  end
 
-def oracle_contract_factory(options = {})
-  options[:json_body] ||= contract_json term: oracle_term_json(options[:term_options] || {})
-  contract_factory options
-end
+  def oracle_term_factory(options = {})
+    oracle_contract_factory.terms.first.tap do |term|
+      term.update_attributes options
+    end
+  end
 
-def escrow_outcome_factory(options = {})
-  term = options[:term] || term_factory
-  result = options[:result] || [EscrowOutcome::FAILURE, EscrowOutcome::SUCCESS].sample
-  transaction_hex = options[:transaction_hex] || options[:transaction_hexes] || [SecureRandom.hex]
+  def contract_factory(options = {})
+    status = options[:status] || Contract::IN_PROGRESS
+    json_body = options[:json_body] || contract_json
+    body = JSON.parse json_body
 
-  EscrowOutcome.create({
-    result: result,
-    term: term,
-    transaction_hexes: transaction_hex
-  })
+    ContractBuilder.perform(body).tap do |contract|
+      contract.status = status
+      contract.save if contract.persisted?
+    end
+  end
+
+  def custom_contract_factory(options = {})
+    options[:json_body] ||= contract_json term: custom_term_json
+    contract_factory options
+  end
+
+  def oracle_contract_factory(options = {})
+    options[:json_body] ||= contract_json term: oracle_term_json(options[:term_options] || {})
+    contract_factory options
+  end
+
+  def escrow_outcome_factory(options = {})
+    term = options[:term] || term_factory
+    result = options[:result] || [EscrowOutcome::FAILURE, EscrowOutcome::SUCCESS].sample
+    transaction_hex = options[:transaction_hex] || options[:transaction_hexes] || [SecureRandom.hex]
+
+    EscrowOutcome.create({
+      result: result,
+      term: term,
+      transaction_hexes: transaction_hex
+    })
+  end
 end
--- spec/support/sample_params.rb
+++ spec/support/sample_params.rb
@@ -1,11 +1,11 @@
+module SpecHelpers
+  def sem_rush_error_50
+    http_response body: 'ERROR 50 :: NOTHING FOUND'
+  end
 
-def sem_rush_error_50
-  http_response body: 'ERROR 50 :: NOTHING FOUND'
-end
-
-def sem_rush_phrase_organic_bitcoin(options = {})
-  domain = options[:domain] || Faker::Internet.domain_name
-http_response body: <<CSV
+  def sem_rush_phrase_organic_bitcoin(options = {})
+    domain = options[:domain] || Faker::Internet.domain_name
+    http_response body: <<CSV
 Domain;Url
 bitcoin.org;https://bitcoin.org/
 #{domain};https://en.wikipedia.org/wiki/Bitcoin
@@ -18,102 +18,103 @@ newyorker.com;http://www.newyorker.com/magazine/2011/10/10/the-crypto-currency
 weusecoins.com;https://www.weusecoins.com/
 cnn.com;http://money.cnn.com/infographic/technology/what-is-bitcoin/
 CSV
-end
+  end
 
-def sem_rush_list_projects
-  [{
-    "keywords": [{
-      "keyword": "smart contract",
-      "tags": [],
-      "timestamp": 1446228184
-    }],
-    "competitors": [],
-    "tools": [],
-    "project_id": 154634,
-    "project_name": "8db311e070582f140d307a36d73ebe21",
-    "url": "smartcontract.com"
-  },
-  {
-    "keywords": [{
-      "keyword": "smart contract",
-      "tags": [],
-      "timestamp": 1446232147
-    }],
-    "competitors": [],
-    "tools": [],
-    "project_id": 154672,
-    "project_name": "787629dec19e0c8f869d19e8766ba8de",
-    "url": "smartcontract.com"
-  }].to_json
-end
-
-def sem_rush_project_response(options = {})
-  key_word = options[:key_word] || Faker::Company.catch_phrase
-  url = options[:url] || Faker::Internet.domain_name
-  name = options[:name] || SecureRandom.name
-  id = options[:id] || Random.rand(1_000_000)
-
-  {
-    "keywords": [
-      {
-        "keyword": key_word,
+  def sem_rush_list_projects
+    [{
+      "keywords": [{
+        "keyword": "smart contract",
+        "tags": [],
+        "timestamp": 1446228184
+      }],
+      "competitors": [],
+      "tools": [],
+      "project_id": 154634,
+      "project_name": "8db311e070582f140d307a36d73ebe21",
+      "url": "smartcontract.com"
+    },
+    {
+      "keywords": [{
+        "keyword": "smart contract",
         "tags": [],
         "timestamp": 1446232147
-      }
-    ],
-    "competitors": [],
-    "tools": [],
-    "project_id": id.to_i,
-    "project_name": name,
-    "url": url
-  }.to_json
-end
+      }],
+      "competitors": [],
+      "tools": [],
+      "project_id": 154672,
+      "project_name": "787629dec19e0c8f869d19e8766ba8de",
+      "url": "smartcontract.com"
+    }].to_json
+  end
 
-def sem_rush_position_tracking(options = {})
-  phrase = options[:phrase] || Faker::Company.catch_phrase
-  ranking = options[:ranking] || "n/a"
-  date = options[:date] || Time.now.utc.strftime("%Y%m%d")
-  domain = "*.#{options[:domain] || Faker::Internet.domain_name}/"
+  def sem_rush_project_response(options = {})
+    key_word = options[:key_word] || Faker::Company.catch_phrase
+    url = options[:url] || Faker::Internet.domain_name
+    name = options[:name] || SecureRandom.name
+    id = options[:id] || Random.rand(1_000_000)
 
-  http_response body: {
-    "data": {
-      "0": {
-        "Be": "",
-        "Cp": "n/a",
-        "Fi": "",
-        "Nq": "n/a",
-        "Ph": phrase,
-        "Pi": "6009114640844893555",
-        "Tg": {},
-        "Dt": {
-          "20151026": { domain => "n/a" },
-          "20151027": { domain => "n/a" },
-          "20151028": { domain => "n/a" },
-          "20151029": { domain => "n/a" },
-          "20151030": { domain => "n/a" },
-          "20151031": { domain => "n/a" },
-          "20151101": { domain => "n/a" },
-          date => { domain => ranking }
-        },
-        "Lt": {
-          "20151026": { domain => "org" },
-          "20151027": { domain => "org" },
-          "20151028": { domain => "org" },
-          "20151029": { domain => "org" },
-          "20151030": { domain => "org" },
-          "20151031": { domain => "org" },
-          "20151101": { domain => "org" },
-          date => { domain => "org"}
-        },
-        "Diff": { domain => ""},
-        "Diff1": { domain => ""},
-        "Diff7": { domain => ""},
-        "Diff30": { domain => ""}
-      }
-    },
-    "limit": "10",
-    "offset": "0",
-    "server": "USA2",
-    "total": "1",
-  }.to_json
+    {
+      "keywords": [
+        {
+          "keyword": key_word,
+          "tags": [],
+          "timestamp": 1446232147
+        }
+      ],
+      "competitors": [],
+      "tools": [],
+      "project_id": id.to_i,
+      "project_name": name,
+      "url": url
+    }.to_json
+  end
+
+  def sem_rush_position_tracking(options = {})
+    phrase = options[:phrase] || Faker::Company.catch_phrase
+    ranking = options[:ranking] || "n/a"
+    date = options[:date] || Time.now.utc.strftime("%Y%m%d")
+    domain = "*.#{options[:domain] || Faker::Internet.domain_name}/"
+
+    http_response body: {
+      "data": {
+        "0": {
+          "Be": "",
+          "Cp": "n/a",
+          "Fi": "",
+          "Nq": "n/a",
+          "Ph": phrase,
+          "Pi": "6009114640844893555",
+          "Tg": {},
+          "Dt": {
+            "20151026": { domain => "n/a" },
+            "20151027": { domain => "n/a" },
+            "20151028": { domain => "n/a" },
+            "20151029": { domain => "n/a" },
+            "20151030": { domain => "n/a" },
+            "20151031": { domain => "n/a" },
+            "20151101": { domain => "n/a" },
+            date => { domain => ranking }
+          },
+          "Lt": {
+            "20151026": { domain => "org" },
+            "20151027": { domain => "org" },
+            "20151028": { domain => "org" },
+            "20151029": { domain => "org" },
+            "20151030": { domain => "org" },
+            "20151031": { domain => "org" },
+            "20151101": { domain => "org" },
+            date => { domain => "org"}
+          },
+          "Diff": { domain => ""},
+          "Diff1": { domain => ""},
+          "Diff7": { domain => ""},
+          "Diff30": { domain => ""}
+        }
+      },
+      "limit": "10",
+      "offset": "0",
+      "server": "USA2",
+      "total": "1",
+    }.to_json
+  end
 end
