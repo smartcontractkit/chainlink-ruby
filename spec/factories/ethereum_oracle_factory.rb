5d988b3449ea6993e5e83836002dcc659996e7e6
Steve Ellis <email@steveell.is>
2016-10-26 13:55:52 -0400

    make oracle factory closer to actual set up

--- spec/factories/ethereum_oracle_factory.rb
+++ spec/factories/ethereum_oracle_factory.rb
@@ -1,9 +1,16 @@
 FactoryGirl.define do
 
   factory :ethereum_oracle do
-    endpoint { "https://#{Faker::Internet.domain_name}/api" }
-    ethereum_contract
-    fields { [SecureRandom.urlsafe_base64] }
+    body do
+      hashie({
+        endpoint: (endpoint.present? ? endpoint : "https://#{Faker::Internet.domain_name}/api"),
+        fields: (fields.present? ? fields : [SecureRandom.urlsafe_base64]),
+      })
+    end
+
+    after(:create) do |oracle|
+      oracle.ethereum_contract(true)
+    end
   end
 
   factory :assigned_ethereum_oracle, parent: :ethereum_oracle do
