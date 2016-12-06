b7d56ea885768ea12288a1cddc7b4cbc5abb222e
Steve Ellis <email@steveell.is>
2016-12-05 21:18:32 -0500

    locally build sol-abi

--- spec/fixtures/ethereum/solidity/oracle.sol
+++ spec/fixtures/ethereum/solidity/oracle.sol
@@ -1,21 +1,45 @@
-contract Oracle {
-  bytes32 currentValue;
-  address creator;
+pragma solidity ^0.4.6;
 
-  function Oracle() {
-    creator = msg.sender;
+contract Owned {
+  address public owner;
+
+  event UpdatedOwner(address to, address from);
+
+  modifier onlyOwner {
+    if (msg.sender == owner) _;
   }
 
-  function update(bytes32 newCurrent) {
-    if (msg.sender != creator) return;
-    currentValue = newCurrent;
+  function Owned() {
+    owner = msg.sender;
   }
 
-  function current() constant returns (bytes32 current) {
-    return currentValue;
+  function setOwner(address newOwner) onlyOwner {
+    if (newOwner != address(0)) {
+      address oldOwner = owner;
+      owner = newOwner;
+      UpdatedOwner(newOwner, oldOwner);
+    }
+  }
+}
+
+contract Oracle is Owned {
+  bytes32 public current;
+  uint public updatedAt;
+  uint requestCounter;
+
+  event Updated(bytes32 current);
+
+  function Oracle(bytes32 newCurrent) {
+    if (newCurrent != bytes32(0)) {
+      current = newCurrent;
+      updatedAt = block.number;
+      Updated(current);
+    }
   }
 
-  function () constant returns (bytes32 current) {
-    return currentValue;
+  function update(bytes32 newCurrent) onlyOwner {
+    current = newCurrent;
+    updatedAt = block.number;
+    Updated(current);
   }
 }
--- spec/models/solidity_client_spec.rb
+++ spec/models/solidity_client_spec.rb
@@ -1,6 +1,5 @@
 describe SolidityClient, type: :model do
-  describe "#compile" do
-    let(:solidity) { SolidityClient.new }
+  describe ".compile" do
     let(:body) { SecureRandom.hex }
 
     it "makes a request to the Solidity service" do
@@ -14,7 +13,17 @@ describe SolidityClient, type: :model do
         })
         .and_return(http_response body: {a: 1}.to_json)
 
-      solidity.compile body
+      SolidityClient.compile body
+    end
+  end
+
+  describe ".sol_abi" do
+    let(:json_abi) { File.read 'spec/fixtures/ethereum/solidity/Oracle.json-abi' }
+    let(:expected_sol_abi) { File.read 'spec/fixtures/ethereum/solidity/Oracle.sol-abi' }
+
+    it "parses an importable Solidity ABI" do
+      sol_abi = SolidityClient.sol_abi 'Oracle', json_abi
+      expect(sol_abi.strip).to eq(expected_sol_abi.strip)
     end
   end
 end
