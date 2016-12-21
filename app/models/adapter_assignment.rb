db6d562d1f35d56587f9a289129ec974ef8c968c
Steve Ellis <email@steveell.is>
2016-12-21 15:18:09 -0500

    rename AdapterAssignment adapter_params to parameters

--- app/models/adapter_assignment.rb
+++ app/models/adapter_assignment.rb
@@ -1,20 +1,20 @@
 class AdapterAssignment < ActiveRecord::Base
 
   belongs_to :adapter, polymorphic: true
-  belongs_to :assignment
+  belongs_to :assignment, inverse_of: :adapter_assignments
 
   validates :adapter, presence: true
   validates :assignment, presence: true
   validates :index, uniqueness: { scope: [:assignment] },
     numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def adapter_params
+  def parameters
     JSON.parse(adapter_params_json) if adapter_params_json.present?
   end
 
-  def adapter_params=(params)
+  def parameters=(params)
     self.adapter_params_json = params ? params.to_json : nil
-    adapter_params
+    parameters
   end
 
 end
--- spec/models/adapter_assignment_spec.rb
+++ spec/models/adapter_assignment_spec.rb
@@ -4,7 +4,7 @@ describe AdapterAssignment, type: :model do
     it { is_expected.to have_valid(:adapter).when(factory_create(:external_adapter), factory_create(:ethereum_oracle), factory_create(:custom_expectation)) }
     it { is_expected.not_to have_valid(:adapter).when(nil) }
 
-    it { is_expected.to have_valid(:adapter_params).when(nil, {}) }
+    it { is_expected.to have_valid(:parameters).when(nil, {}, {SecureRandom.hex => SecureRandom.hex}) }
 
     it { is_expected.to have_valid(:assignment).when(factory_create(:assignment)) }
     it { is_expected.not_to have_valid(:assignment).when(nil) }
