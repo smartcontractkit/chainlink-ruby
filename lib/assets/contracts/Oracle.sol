73031bee99c6f91063fd76d78c0f0c72220bc1d6
Steve Ellis <email@steveell.is>
2017-02-16 02:43:26 -0500

    seperate out Owned contract

--- lib/assets/contracts/Oracle.sol
+++ lib/assets/contracts/Oracle.sol
@@ -1,26 +1,6 @@
 pragma solidity ^0.4.6;
 
-contract Owned {
-  address public owner;
-
-  event UpdatedOwner(address to, address from);
-
-  modifier onlyOwner {
-    if (msg.sender == owner) _;
-  }
-
-  function Owned() {
-    owner = msg.sender;
-  }
-
-  function setOwner(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-      address oldOwner = owner;
-      owner = newOwner;
-      UpdatedOwner(newOwner, oldOwner);
-    }
-  }
-}
+import 'Owned.sol';
 
 contract Oracle is Owned {
   bytes32 public current;
--- lib/assets/contracts/Uint256Oracle.sol
+++ lib/assets/contracts/Uint256Oracle.sol
@@ -1,26 +1,6 @@
 pragma solidity ^0.4.6;
 
-contract Owned {
-  address public owner;
-
-  event UpdatedOwner(address to, address from);
-
-  modifier onlyOwner {
-    if (msg.sender == owner) _;
-  }
-
-  function Owned() {
-    owner = msg.sender;
-  }
-
-  function setOwner(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-      address oldOwner = owner;
-      owner = newOwner;
-      UpdatedOwner(newOwner, oldOwner);
-    }
-  }
-}
+import 'Owned.sol';
 
 contract Uint256Oracle is Owned {
   uint256 public current;
