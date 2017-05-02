8840b835d3b13cf9aa61ff1d8c2323b866bc9fc9
Steve Ellis <email@steveell.is>
2017-05-02 16:02:58 -0700

    require term has expectation

--- spec/factories/term_factory.rb
+++ spec/factories/term_factory.rb
@@ -7,6 +7,7 @@ FactoryGirl.define do
   factory :term do
     contract
     end_at { (rand(365) + 1).days.from_now }
+    expectation { factory_build(:assignment) }
     name { generate(:name) }
     start_at { Time.now }
   end
