445a619abae8891eaeed09e3af11c15211630949
Steve Ellis <email@steveell.is>
2017-10-30 14:54:33 -0700

    prevent initializing constants multiple times

--- app/models/ethereum.rb
+++ app/models/ethereum.rb
@@ -1,7 +1,7 @@
 module Ethereum
-  WEI_PER_ETHER = 10**18
-  NULL_ACCOUNT = "0x#{'0' * 40}"
-  EMPTY_BYTE = "\x00".encode('utf-8', 'utf-8', invalid: :replace)
+  WEI_PER_ETHER ||= 10**18
+  NULL_ACCOUNT ||= "0x#{'0' * 40}"
+  EMPTY_BYTE ||= "\x00".encode('utf-8', 'utf-8', invalid: :replace)
 
   def self.table_name_prefix
     'ethereum_'
